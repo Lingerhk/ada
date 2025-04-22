4ba0634e452fff183add72eefe529fc0d864e34f
lingerhk <lingerhk@gmail.com>
2025-04-22 21:49:55 +0800

    optimization dir of sensor

---
+++ agent/sensor/tools/dec_cfg.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"ada/infra/crypto"
+	"encoding/base64"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	confFile := "./sensor-3.yaml"
+	confEncFile := "./sensor-3.cfg"
+	const cfgEncKey = "adcc368715ce1bd2"
+
+	var fileCnt []byte
+	fileEncCntB64, err := os.ReadFile(confEncFile)
+	if err != nil {
+		panic(err)
+	}
+
+	sDec, err := base64.StdEncoding.DecodeString(string(fileEncCntB64))
+	if err != nil {
+		panic(err)
+	}
+	aes := crypto.NewAes([]byte(cfgEncKey))
+	fileCnt, err = aes.Decrypt(sDec)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(confFile, fileCnt, 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("sensor.yml decrypted ok!")
+	time.Sleep(2 * time.Second)
+}
---
+++ agent/sensor/tools/enc_cfg.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"ada/infra/crypto"
+	"encoding/base64"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	confFile := "./sensor.yaml"
+	confEncFile := "./sensor.cfg"
+	const cfgEncKey = "adcc368715ce1bd2"
+
+	var fileCnt, fileEncCnt []byte
+	fileCnt, err := os.ReadFile(confFile)
+	if err != nil {
+		panic(err)
+	}
+
+	aes := crypto.NewAes([]byte(cfgEncKey))
+	fileEncCnt, err = aes.Encrypt(string(fileCnt))
+	if err != nil {
+		panic(err)
+	}
+
+	fileEncCntB64 := base64.StdEncoding.EncodeToString(fileEncCnt)
+
+	err = os.WriteFile(confEncFile, []byte(fileEncCntB64), 0644)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("sensor.cfg encrypted ok!")
+	time.Sleep(2 * time.Second)
+}
