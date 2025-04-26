4fb867e251a5a457689822ef038403b155672588
lingerhk <lingerhk@gmail.com>
2025-04-26 11:05:14 +0800

    add sensor builder script

--- agent/sensor/cmd/make.bat
+++ agent/sensor/cmd/make.bat
@@ -3,13 +3,36 @@ echo ADA Sensor Build Script
 echo ==========================================
 set BuildName=ADA@sensor
 set Version=2.6.3
+for /f "tokens=1* delims==" %%a in ('wmic os get LocalDateTime /value') do if "%%a"=="LocalDateTime" set datetime=%%b
+set BuildTime=%datetime:~0,8%_%datetime:~8,6%
 set VersionPath=ada/infra/version
+set TargetBin=ada_sensor.exe
 echo Build Version: %Version%
 echo Build Name: %BuildName%
+echo Build Time: %BuildTime%
 echo ==========================================
 
-echo Go build ada_sensor.exe
-go build -o ada_sensor.exe -ldflags "-s -w -X %VersionPath%.BuildName=%BuildName% -X %VersionPath%.BuildVersion=%Version%" sensor.go
-echo Build ada_sensor.exe success!
+::echo Download dependencies(we using vendor mode)
+::go mod download
 
-timeout /T 5
+echo Build binary
+set GobuildOptions=-ldflags "-s -w -X %VersionPath%.BuildName=%BuildName% -X %VersionPath%.BuildVersion=%Version% -X %VersionPath%.BuildTime=%BuildTime%"
+del %TargetBin% > nul 2>&1
+
+
+:: check if vendor folder exists
+set vendorPath=../../../vendor
+if exist %vendorPath% (
+    echo Vendor folder exists, using vendor mode
+    set GobuildOptions=-mod=vendor %GobuildOptions%
+)
+
+go build %GobuildOptions% -o %TargetBin% sensor.go
+
+:: check ada_sensor.exe
+if not exist %TargetBin% (
+    echo Build failed, %TargetBin% not found!
+    exit /b 1
+)
+
+echo Build %TargetBin% success!
