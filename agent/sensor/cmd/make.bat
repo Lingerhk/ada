a7ed9c9eac02cc746584f5c94cecc4d04f139b49
lingerhk <lingerhk@gmail.com>
2025-05-15 23:11:51 +0800

    add powershell sensor install script

--- agent/sensor/cmd/make.bat
+++ agent/sensor/cmd/make.bat
@@ -2,11 +2,11 @@
 echo ADA Sensor Build Script
 echo ==========================================
 set BuildName=ADA@sensor
-set Version=2.6.3
+set Version=2.6.5
 for /f "tokens=1* delims==" %%a in ('wmic os get LocalDateTime /value') do if "%%a"=="LocalDateTime" set datetime=%%b
 set BuildTime=%datetime:~0,8%_%datetime:~8,6%
 set VersionPath=ada/infra/version
-set TargetBin=ada_sensor.exe
+set TargetBin=adaegis.exe
 echo Build Version: %Version%
 echo Build Name: %BuildName%
 echo Build Time: %BuildTime%
@@ -23,7 +23,9 @@ del %TargetBin% > nul 2>&1
 :: check if vendor folder exists
 set vendorPath=../../../vendor
 if exist %vendorPath% (
-    echo Vendor folder exists, using vendor mode
+    echo "Vendor folder exists, using vendor mode"
+    echo "update: go mod vendor..."
+    go mod vendor
     set GobuildOptions=-mod=vendor %GobuildOptions%
 )
 
@@ -32,6 +34,7 @@ go build %GobuildOptions% -o %TargetBin% sensor.go
 :: check ada_sensor.exe
 if not exist %TargetBin% (
     echo Build failed, %TargetBin% not found!
+    sleep 3
     exit /b 1
 )
 
