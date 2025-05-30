de90c83dda3740627ac706b3b2e5b7bb1217335b
lingerhk <lingerhk@gmail.com>
2025-05-30 18:06:58 +0800

    docker optimization

--- script/docker/build.sh
+++ script/docker/build.sh
@@ -11,20 +11,23 @@ build_engine() {
     cp ${ada_path}/bin/engine ./
     cp ${ada_path}/bin/engine.yaml ./
     sed -i "s/version=.*/version=${version}/g" Dockerfile
-    docker build -f Dockerfile -t ada_engine:${version} .
+    docker build --network=host -f Dockerfile -t ada_engine:${version} .
     cd - || exit
 }
 
 # build backend
 build_backend() {
     cd backend || exit;
+    cp ${ada_path}/agent/script/adaegis.zip ./
+    cp ${ada_path}/agent/script/install-adaegis.ps1 ./
+    cp ${ada_path}/agent/script/uninstall-adaegis.ps1 ./
     cp ${ada_path}/bin/apiserver ./
     cp ${ada_path}/bin/task_server ./
     cp ${ada_path}/bin/task_worker ./
     cp ${ada_path}/bin/tasker.yaml ./
     cp ${ada_path}/bin/apiserver.yaml ./
     sed -i "s/version=.*/version=${version}/g" Dockerfile
-    docker build -f Dockerfile -t ada_backend:${version} .
+    docker build --network=host -f Dockerfile -t ada_backend:${version} .
     cd - || exit
 }
 
@@ -34,46 +37,65 @@ build_scanner() {
     cp ${ada_path}/bin/scanner ./
     cp ${ada_path}/bin/scanner.yaml ./
     sed -i "s/version=.*/version=${version}/g" Dockerfile
-    docker build -f Dockerfile -t ada_scanner:${version} .
+    docker build --network=host -f Dockerfile -t ada_scanner:${version} .
     cd - || exit
 }
 
 # build zeek
 build_zeek() {
     cd zeek || exit;
-    docker build -f Dockerfile -t ada_zeek:${version} .
+    docker build --network=host -f Dockerfile -t ada_zeek:${version} .
     cd - || exit
 }
 
 # build redis
 build_redis() {
     cd redis || exit;
-    docker build -f Dockerfile -t ada_redis:${version} .
+    docker build --network=host -f Dockerfile -t ada_redis:${version} .
     cd - || exit
 }
 
 # build mongodb
 build_mongodb() {
     cd mongodb || exit;
-    # TODO: cp 02-init_db.js to docker/mongodb/02-init_db.js
-    docker build -f Dockerfile -t ada_mongodb:${version} .
+    sed -i "s/ADA_SERVER_VERSION/${version}/g" 02-init_db.js
+    docker build --network=host -f Dockerfile -t ada_mongodb:${version} .
     cd - || exit
 }
 
 #build kibana
 build_kibana() {
     cd kibana || exit;
-    docker build -f Dockerfile -t ada_kibana:${version} .
+    docker build --network=host -f Dockerfile -t ada_kibana:${version} .
     cd - || exit
 }
 
 # build elasticsearch
 build_elasticsearch() {
     cd elasticsearch || exit;
-    docker build -f Dockerfile -t ada_elasticsearch:${version} .
+    docker build --network=host -f Dockerfile -t ada_elasticsearch:${version} .
     cd - || exit
 }
 
+package_images() {
+    echo "package images:"
+    echo "packing ada_engine:${version}.tar"
+    docker save -o ada_engine:${version}.tar ada_engine:${version}
+    echo "packing ada_backend:${version}.tar"
+    docker save -o ada_backend:${version}.tar ada_backend:${version}
+    echo "packing ada_scanner:${version}.tar"
+    docker save -o ada_scanner:${version}.tar ada_scanner:${version}
+    echo "packing ada_zeek:${version}.tar"
+    docker save -o ada_zeek:${version}.tar ada_zeek:${version}
+    echo "packing ada_redis:${version}.tar"
+    docker save -o ada_redis:${version}.tar ada_redis:${version}
+    echo "packing ada_mongodb:${version}.tar"
+    docker save -o ada_mongodb:${version}.tar ada_mongodb:${version}
+    echo "packing ada_kibana:${version}.tar"
+    docker save -o ada_kibana:${version}.tar ada_kibana:${version}
+    echo "packing ada_elasticsearch:${version}.tar"
+    docker save -o ada_elasticsearch:${version}.tar ada_elasticsearch:${version}
+}
 
 main() {
     case $1 in
@@ -111,8 +133,11 @@ main() {
             build_kibana
             build_elasticsearch
             ;;
+        package)
+            package_images
+            ;;
         *)
-            echo "Usage: $0 {engine|backend|scanner|zeek|redis|mongodb|kibana|elasticsearch|all}"
+            echo "Usage: $0 {engine|backend|scanner|zeek|redis|mongodb|kibana|elasticsearch|all|package}"
             exit 1
             ;;
     esac
