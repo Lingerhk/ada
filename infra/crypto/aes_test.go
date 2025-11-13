515ef4cd3f57f61824e4845e0878e3b70e86c19d
Lingerhk <lingerhk@gmail.com>
2025-11-13 05:22:54 +0000

    Update aes_test.go

--- infra/crypto/aes_test.go
+++ infra/crypto/aes_test.go
@@ -1,15 +1,26 @@
 package crypto
 
 import (
+	"encoding/base64"
 	"fmt"
 	"testing"
 )
 
 func TestAes(t *testing.T) {
-	aes := &Aes{cipher: []byte("abcdef0123456789")}
+	aes := &Aes{cipher: []byte("3a43d7a31b3ca37d")}
 
 	data, _ := aes.Encrypt("abcdef")
 	str := string(data)
 	fmt.Println("encrypt:", data)
 	fmt.Println("len:", len(str))
+
+	// Decrypt the data
+	encByte, err := base64.StdEncoding.DecodeString("DykZr1+oybvC+E+Kmtz4lA==")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	decryptedData, _ := aes.Decrypt(encByte)
+	fmt.Printf("decrypt: %s\n", decryptedData)
+	fmt.Println("len:", len(decryptedData))
 }
